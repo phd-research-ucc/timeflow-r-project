855384868b11e6cf5daec60d203f881d597b73b2
Oleksii Dovhaniuk <alexdovhaniuk@gmail.com>
2023-11-28 09:17:19 +0200

    2023-11-27 try new charts

--- src/scripts/01_setup.R
+++ src/scripts/01_setup.R
@@ -18,7 +18,7 @@ library(glue)
 # Read Files ---------------------------------------------------------------
 
 raw_df <- read.csv(
-  'src/data/raw/2023-11-26_timeflow_main.csv', 
+  'src/data/raw/2023-11-27_timeflow_main.csv', 
   header = TRUE, 
   skip = 1
 ) |>
@@ -48,8 +48,11 @@ restored_df <- raw_df |>
 # Analytics ---------------------------------------------------------------
 
 summary_df <- restored_df |>
-  group_by(date, tag1) |>
-  summarise(total_time = sum(time_min))
+  group_by(date) |>
+  summarise(
+    hours = sum(time_min) %/% 60,
+    minutes = sum(time_min) %% 60
+  )
 
 
 
@@ -57,13 +60,61 @@ summary_df <- restored_df |>
 
 # Charts ------------------------------------------------------------------
 
+# Bar chart
+bar_chart_df <- restored_df |>
+  group_by(date, tag1) |>
+  summarise( total_min = sum(time_min) )
+
 ggplot(
-  summary_df, 
-  aes(x = date, y = total_time, fill = tag1) ) +
+  bar_chart_df, 
+  aes(x = date, y = total_min, fill = tag1) ) +
   geom_col(position = "stack", width = 0.7) +
-  geom_text(aes(label = total_time), position = position_stack(vjust = 0.5), size = 3) +
+  geom_text(aes(label = total_min), position = position_stack(vjust = 0.5), size = 3) +
   labs(title = "Total Time for Each Activity per Day",
        x = "Date",
        y = "Total Time (minutes)",
        fill = "Activity") +
   theme_minimal()
+
+
+# Pie chart
+pie_chart_df <- restored_df |>
+  group_by(tag1) |>
+  summarise( total_min = sum(time_min) )
+
+ggplot(pie_chart_df, aes(x = "", y = total_min, fill = tag1)) +
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  coord_polar(theta = "y") +
+  theme_minimal() +
+  labs(title = "Time spent distribution among activities",
+       fill = "Category") +
+  theme(legend.position = "right")
+
+
+# Heat map
+heat_map_df <- restored_df |>
+  group_by(date) |>
+  summarise( total_min = sum(time_min) )
+
+
+# # Example data frame
+# your_data <- data.frame(
+#   Month = rep(c("Jan", "Feb", "Mar", "Apr", "May", "Jun"), each = 3),
+#   Activity = rep(c("A", "B", "C"), times = 6),
+#   Value = c(10, 15, 8, 20, 25, 18, 12, 8, 15, 22, 30, 25, 14, 18, 10, 12, 28, 20)
+# )
+# 
+# your_data_wide <- your_data %>%
+#   pivot_wider(names_from = Activity, values_from = Value)
+# 
+# ggplot(
+#   your_data_wide, 
+#   aes(x = Month, y = Activity, fill = A)
+#   ) +
+#   geom_tile(color = "white", linewidth = 0.5) +
+#   scale_fill_viridis_c() +  # You can use other color scales from viridis package
+#   theme_minimal() +
+#   labs(title = "Monthly Activity Heatmap",
+#        x = "Month",
+#        y = "Activity",
+#        fill = "Value")
